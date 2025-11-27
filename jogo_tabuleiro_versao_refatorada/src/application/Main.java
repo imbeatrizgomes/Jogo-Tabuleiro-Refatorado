007f4b63354f55af7c35cbaecac2467161e4163d
Beatriz <anabeatrizsantos3830@gmail.com>
2025-11-27 08:42:50 -0300

    Update Main.java

--- jogo_tabuleiro_versao_refatorada/src/application/Main.java
+++ jogo_tabuleiro_versao_refatorada/src/application/Main.java
@@ -3,6 +3,7 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
 import entities.Jogador;
 import entities.JogadorComSorte;
 import entities.JogadorNormal;
@@ -18,28 +19,32 @@ public class Main {
         System.out.print("Modo DEBUG (S/N)? ");
         String resp = sc.next();
         boolean debug = resp.equalsIgnoreCase("S");
-        sc.nextLine(); 
+        sc.nextLine();
 
         System.out.print("Quantos jogadores participarão (2 a 6)? ");
         int qtd = sc.nextInt();
-        sc.nextLine();
+        sc.nextLine(); // limpa ENTER
+
         if (qtd < 2) qtd = 2; 
         if (qtd > 6) qtd = 6;
-
 
         List<Jogador> jogadores = new ArrayList<>();
 
         for (int i = 1; i <= qtd; i++) {
+
             System.out.println("\nJogador " + i + ":");
+
             System.out.print("Nome: ");
             String nome = sc.nextLine();
+
             System.out.print("Cor: ");
             String cor = sc.nextLine();
-            
+
             System.out.println("Tipo 1 - Normal");
             System.out.println("Tipo 2 - Sortudo");
             System.out.println("Tipo 3 - Azarado");
             System.out.print("Escolha uma das opções (1/2/3): ");
+
             String tipo = sc.next();
             sc.nextLine();
 
@@ -51,15 +56,16 @@ public class Main {
             } else {
                 p = new JogadorNormal(nome, cor);
             }
+
             jogadores.add(p);
         }
 
         Tabuleiro tabuleiro = new Tabuleiro(jogadores);
         JogoRegras regras = new JogoRegras(tabuleiro.jogadores());
         Jogo jogo = new Jogo(tabuleiro, regras, debug);
+
         jogo.loopDoJogo();
 
         sc.close();
-    
     }
 }
